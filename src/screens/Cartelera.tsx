376b2189ade314f7634bdf6156eea3c93093a55e
Gabriel <gabrielcorreacardenas@gmail.com>
2023-06-15 01:01:00 -0500

    chore: remove commented mock data

--- src/screens/Cartelera.tsx
+++ src/screens/Cartelera.tsx
@@ -33,31 +33,6 @@ export function Billboard() {
                 )
               })
             }
-              
-            {/* <Grid.Col span={3}>
-              <Moca title={"Transformers"} link={"https://movies.universalpictures.com/media/fstx-montage1sheet2-rgb-1-64108e93e63dd-1.jpg"} />
-            </Grid.Col>
-            <Grid.Col span={3}>
-              <Moca title={"Duro de matar"} link={"https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTpVXKGjwHSAeEqBH3EjoBipDnB7azbLUX93bL7VHEAJmOnPXR6"} />
-            </Grid.Col>
-            <Grid.Col span={3}>
-              <Moca title={"Enredados"} link={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqjiLiIFJaZSqEAtCCT0N1bw1_Wm9oJHVvHHASpr4qdg3TitCB"} />
-            </Grid.Col>
-            <Grid.Col span={3}>
-            <Moca title={"Boogeyman"} link={"https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQbMtZfdH5eQC-YV_uDer35C-yoAPFrVMZWbeZMFTH-GuLf2ibj"} />
-            </Grid.Col>
-            <Grid.Col span={3}>
-              <Moca title={"Rápido y furioso"} link={"https://movies.universalpictures.com/media/fstx-montage1sheet2-rgb-1-64108e93e63dd-1.jpg"} />
-            </Grid.Col>
-            <Grid.Col span={3}>
-              <Moca title={"Duro de matar"} link={"https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTpVXKGjwHSAeEqBH3EjoBipDnB7azbLUX93bL7VHEAJmOnPXR6"} />
-            </Grid.Col>
-            <Grid.Col span={3}>
-            <Moca title={"Boogeyman"} link={"https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQbMtZfdH5eQC-YV_uDer35C-yoAPFrVMZWbeZMFTH-GuLf2ibj"} />
-            </Grid.Col>
-            <Grid.Col span={3}>
-              <Moca title={"Enredados"} link={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqjiLiIFJaZSqEAtCCT0N1bw1_Wm9oJHVvHHASpr4qdg3TitCB"} />
-            </Grid.Col> */}
           </Grid>
       </div>
     </div>
